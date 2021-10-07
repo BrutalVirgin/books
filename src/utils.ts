7883c4819708a83b6889f7f49bd595da776e7e39
Kirill Bondarenko <kirill.bondarenko@timecode.com>
2021-10-07 22:33:39 +0300

    updates

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,10 @@
 // todo fix too large number issue
 export const genId = () => Math.round(Math.random() * 1000000000000000)
+
+
+export const user1 = (user: string) => user.replace(/@\{(\w+)\}/g, (match, group) => {
+    if (group === 'currentdate') {
+        return new Date();
+    }
+})
+
